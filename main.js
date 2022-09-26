ba3370263cc3342e3becca5282dcf8c76927ff1f
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-09-26 15:12:10 -0300

    updated proj: add a new feat with name 'slider-scroll' to slider-motos section

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import SliderInfinite from "./_source/script/slider-infinite.js";
+import SliderScroll from "./_source/script/slider-scroll.js";
 
 const sliderSubheader = new SliderInfinite(
   "[data-slider=infinite-slider]",
   ".__slider-contents"
 );
-
 sliderSubheader.init();
+
+const sliderMotos = new SliderScroll(
+  "[data-carrossel=motos]",
+  ".__slider-items"
+);
+sliderMotos.init();
