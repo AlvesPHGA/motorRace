cfbd9865ca6478b722e3d1ac8408076582dba2b4
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-10-05 10:57:20 -0300

    updated proj: add a feature to project to menu hamburger

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ import SliderScroll from "./_source/script/slider-scroll.js";
 
 import SliderNavigator from "./_source/script/slider-show.js";
 
-const sliderSubheader = new SliderInfinite(
-  "[data-slider=infinite-slider]",
-  ".__slider-contents"
-);
-sliderSubheader.init();
+// const sliderSubheader = new SliderInfinite(
+//   "[data-slider=infinite-slider]",
+//   ".__slider-contents"
+// );
+// sliderSubheader.init();
 
-const sliderMotos = new SliderScroll(
-  "[data-carrossel=motos]",
-  ".__slider-items"
-);
-sliderMotos.init();
+// const sliderMotos = new SliderScroll(
+//   "[data-carrossel=motos]",
+//   ".__slider-items"
+// );
+// sliderMotos.init();
 
-const sliderBrandsMobile = new SliderNavigator(
-  "[data-slider=show-brands]",
-  ".__content-slider",
-  ".__slider-wrapp"
-);
-sliderBrandsMobile.init();
+// const sliderBrandsMobile = new SliderNavigator(
+//   "[data-slider=show-brands]",
+//   ".__content-slider",
+//   ".__slider-wrapp"
+// );
+// sliderBrandsMobile.init();
---
+++ script/active-menu-hanburger.js
@@ -0,0 +1,9 @@
+export default class ActiveMenuHamburger {
+  constructor(element) {
+    this.element = document.querySelector(element);
+  }
+
+  init() {
+    return this;
+  }
+}
