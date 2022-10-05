1b72caa9319d1dbdbe98e3f2759c6afb9dbe8943
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-10-05 16:52:03 -0300

    updated proj: updated active menu class (js)

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-import SliderInfinite from "./_source/script/slider-infinite.js";
+import ActiveMenuHamburger from "./script/active-menu-hanburger.js";
 
-import SliderScroll from "./_source/script/slider-scroll.js";
-
-import SliderNavigator from "./_source/script/slider-show.js";
-
-// const sliderSubheader = new SliderInfinite(
-//   "[data-slider=infinite-slider]",
-//   ".__slider-contents"
-// );
-// sliderSubheader.init();
-
-// const sliderMotos = new SliderScroll(
-//   "[data-carrossel=motos]",
-//   ".__slider-items"
-// );
-// sliderMotos.init();
-
-// const sliderBrandsMobile = new SliderNavigator(
-//   "[data-slider=show-brands]",
-//   ".__content-slider",
-//   ".__slider-wrapp"
-// );
-// sliderBrandsMobile.init();
+const menu_hanburger = new ActiveMenuHamburger(".__menu-hamburger");
+menu_hanburger.init();
--- script/active-menu-hanburger.js
+++ script/active-menu-hanburger.js
@@ -3,7 +3,25 @@ export default class ActiveMenuHamburger {
     this.element = document.querySelector(element);
   }
 
+  eventClick() {
+    this.element.addEventListener("click", this.onOff);
+  }
+
+  onOff() {
+    const box = document.querySelector(".__box");
+    const menu = document.querySelector(".__links");
+
+    box.classList.toggle("__active");
+    menu.classList.toggle("__active-menu-mobile");
+  }
+
+  eventBind() {
+    this.onOff = this.onOff.bind(this);
+  }
+
   init() {
+    this.eventBind();
+    this.eventClick();
     return this;
   }
 }
