21d8db4c9a4c0fdc57e8c99b3e6334af0edb2e9d
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-10-08 12:12:17 -0300

    updated proj: add a new feature to project with name slider-scroll.js, it is to slider-motos

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 import ActiveMenuHamburger from "./script/active-menu-hanburger.js";
 import AnimaScroll from "./script/anima-scroll.js";
+import SliderScroll from "./script/slider-scroll.js";
 
 const menu_hanburger = new ActiveMenuHamburger(".__menu-hamburger");
 menu_hanburger.init();
 
 const anima_section = new AnimaScroll("[data-anima]");
 anima_section.init();
+
+const slider_scroll = new SliderScroll(
+  "[data-slider=scroll]",
+  ".__slider-content"
+);
+slider_scroll.init();
