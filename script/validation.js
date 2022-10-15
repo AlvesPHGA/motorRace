224334b34bed37cdb97b3864bb61c219d7131ba4
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-10-15 15:07:54 -0300

    updated proj: updated feature to form

--- script/validation.js
+++ script/validation.js
@@ -12,7 +12,35 @@ export default class Validation {
 
   inputs() {
     const fields = this.validation_form.querySelectorAll("[data-required]");
-    console.log(fields);
+
+    fields.forEach((f) => {
+      if (!f.value) {
+        f.classList.add("__error");
+      } else {
+        this.validField(f);
+      }
+    });
+  }
+
+  validField(field) {
+    if (field.name === "name") this.validName(field);
+    if (field.name === "email") this.validEmail(field);
+    if (field.name === "phone") this.validPhone(field);
+  }
+
+  validName(element) {
+    const dataName = element.value;
+
+    if (dataName.length < 3) element.classList.add("__error");
+  }
+  validEmail(element) {
+    const dataEmail = element.value;
+    const elRegex = /(\w+)\S@(\w{2,}).(\w{3,})/;
+
+    if (!dataEmail.match(elRegex)) element.classList.add("__error");
+  }
+  validPhone(element) {
+    console.log(element);
   }
 
   init() {
