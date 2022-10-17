5e873cec1f9c24e3f85bdf6584470c0ffaa39f44
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-10-17 15:00:14 -0300

    updated proj: updated form in contact section

--- script/validation.js
+++ script/validation.js
@@ -3,48 +3,98 @@ export default class Validation {
     this.validation_form = document.querySelector(validation_form);
   }
 
-  validForm() {
+  event() {
     this.validation_form.addEventListener("submit", (ev) => {
-      ev.preventDefault();
-      this.inputs();
+      this.handleSubmit(ev);
     });
   }
 
-  inputs() {
-    const fields = this.validation_form.querySelectorAll("[data-required]");
+  handleSubmit(ev) {
+    ev.preventDefault();
+    const validFields = this.validField();
 
-    fields.forEach((f) => {
-      if (!f.value) {
-        f.classList.add("__error");
+    if (validFields) {
+      alert("Formulario enviado com sucesso");
+      this.validation_form.reset();
+    }
+  }
+
+  validField() {
+    const fields = this.validation_form.querySelectorAll("[data-required]");
+    const errors = this.validation_form.querySelectorAll(".__error");
+
+    let valid = true;
+
+    errors.forEach((er) => er.classList.remove("__error"));
+
+    fields.forEach((field) => {
+      if (!field.value) {
+        field.classList.add("__error");
+        valid = false;
       } else {
-        this.validField(f);
+        if (field.name === "name") {
+          if (!this.validName(field)) valid = false;
+        }
+        if (field.name === "email") {
+          if (!this.validEmail(field)) valid = false;
+        }
+        if (field.name === "phone") {
+          if (!this.validPhone(field)) valid = false;
+        }
       }
     });
-  }
 
-  validField(field) {
-    if (field.name === "name") this.validName(field);
-    if (field.name === "email") this.validEmail(field);
-    if (field.name === "phone") this.validPhone(field);
+    return valid;
   }
 
   validName(element) {
     const dataName = element.value;
 
-    if (dataName.length < 3) element.classList.add("__error");
+    let valid = true;
+
+    if (dataName.length < 3) {
+      element.classList.add("__error");
+      valid = false;
+    }
+
+    return valid;
   }
+
   validEmail(element) {
     const dataEmail = element.value;
     const elRegex = /(\w+)\S@(\w{2,}).(\w{3,})/;
 
-    if (!dataEmail.match(elRegex)) element.classList.add("__error");
+    let valid = true;
+
+    if (!dataEmail.match(elRegex)) {
+      element.classList.add("__error");
+      valid = false;
+    }
+
+    return valid;
   }
+
   validPhone(element) {
-    console.log(element);
+    const dataPhone = element.value;
+    const elRegex = /(\(?[1-9]{2}\)?)(\-|\s)?(9?[2-9]{4})(-|\s)?([0-9]{4})/;
+
+    let valid = true;
+
+    if (!dataPhone.match(elRegex)) {
+      element.classList.add("__error");
+      valid = false;
+    }
+
+    return valid;
+  }
+
+  bindEvent() {
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   init() {
-    this.validForm();
+    this.bindEvent();
+    this.event();
     return this;
   }
 }
